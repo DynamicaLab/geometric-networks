a07028f959b7dcc6cb8374718d96fd2d9efb0f89
antoineallard <antoine.allard.1@gmail.com>
2024-03-05 21:18:03 -0500

    première version du double-edge swap

---
+++ analysis/double-edge-swap.py
@@ -0,0 +1,150 @@
+import numpy as np
+
+
+
+def double_edge_swap_on_unweighted_graphs(adjacency_matrix, number_of_iterations=None):
+
+    # Checks is the graph is undirected
+    if numpy.allclose(adjacency_matrix, adjacency_matrix.T):
+        undirected = True
+    else:
+        undirected = False
+
+    # Creates a list of edges (should be faster than working with the adjacency matrix)
+    if undirected:
+        edgelist = np.argwhere(np.triu(adjacency_matrix))
+    else:
+        edgelist = np.argwhere(adjacency_matrix)
+    number_of_edges = edgelist.shape[0]
+
+    # Sets the number of iterations if none is provided
+    if number_of_iterations == None:
+        number_of_iterations = 10*number_of_edges
+
+    # Performs the double-edge swap algorithm
+    for nb_iter in range(number_of_iterations):
+        
+        # Chooses two distinct edges at random and identifies the vertices
+        e1, e2 = np.random.choice(number_of_edges, 2, replace=False)
+        target1, source1 = edgelist[e1, 0], edgelist[e1, 1]
+        target2, source2 = edgelist[e2, 0], edgelist[e2, 1]
+
+        # Randomly choose one of the two possible swaps
+        if undirected:
+            if np.random.rand() < 0.5:
+                source2, target2 = target2, source2
+
+        # Check if the swap would leave the graph space
+        if target1 == source2:  # Would create a self-loop
+            continue
+        if target2 == source1:  # Would create a self-loop
+            continue
+        if adjacency_matrix[target1, source2] == 1:  # Would create a parallel edge
+            continue
+        if adjacency_matrix[target2, source1] == 1:  # Would create a parallel edge
+            continue
+
+        # Performs the swap by updating the adjacency matrix and the edgelist
+        adjacency_matrix[target1, source1] = 0
+        adjacency_matrix[target2, source2] = 0
+        if undirected:
+            adjacency_matrix[source1, target1] = 0
+            adjacency_matrix[source2, target2] = 0
+
+        adjacency_matrix[target1, source2] = 0
+        adjacency_matrix[target2, source1] = 0
+        if undirected:
+            adjacency_matrix[source2, target1] = 0
+            adjacency_matrix[source1, target2] = 0
+
+        edgelist[e1, 0], edgelist[e1, 1] = target1, source2
+        edgelist[e2, 0], edgelist[e2, 1] = target2, source1
+    
+    return adjacency_matrix
+
+
+
+
+# def within_distance_interval(edge1, edge2, distance_matrix, min_distance, max_distance):
+#     if distance_matrix != None:
+#         if min_distance != None:
+#             if distance_matrix[edge1[0], edge1[1]] < min_distance:
+#                 return False
+#             if distance_matrix[edge2[0], edge2[1]] < min_distance:
+#                 return False
+#             if distance_matrix[edge1[0], edge2[1]] < min_distance:
+#                 return False
+#             if distance_matrix[edge2[0], edge1[1]] < min_distance:
+#                 return False
+#         if max_distance != None:
+#             if distance_matrix[edge1[0], edge1[1]] > max_distance:
+#                 return False
+#             if distance_matrix[edge2[0], edge2[1]] > max_distance:
+#                 return False
+#             if distance_matrix[edge1[0], edge2[1]] > max_distance:
+#                 return False
+#             if distance_matrix[edge2[0], edge1[1]] > max_distance:
+#                 return False
+#     return True
+
+
+# def double_edge_swap(adjacency_matrix, number_of_iterations=None, directed=False, preserve_out_strength=True, distance_matrix=None, min_distance=None, max_distance=None):
+
+#     if not directed:
+#         adjacency_matrix = np.triu(adjacency_matrix)
+
+#     number_of_iterations_completed = 0
+#     while number_of_iterations_completed < number_of_iterations:
+
+#         # Choose two distinct edges at random
+#         edges = np.argwhere(adjacency_matrix)
+#         edge1 = np.random.choice(edges)
+#         edge2 = np.random.choice(edges)
+#         while np.allclose(edge1, edge2):
+#             edge2 = np.random.choice(edges)
+#         weight1 = adjacency_matrix[edge1[0], edge1[1]]
+#         weight2 = adjacency_matrix[edge2[0], edge2[1]]
+
+#         # Randomly choose one of the two possible swaps if the graph is undirected
+#         if not directed:
+#             if np.random.rand() < 0.5:
+#                 edge2[0], edge2[1] = edge2[1], edge2[0]
+
+#         # Check that the old and new edges are within a prescribed distance interval
+#         if distance_matrix != None:
+#             if not within_distance_interval(edge1, edge2, distance_matrix, min_distance, max_distance):
+#                 continue
+
+
+#         number_of_iterations_completed += 1
+
+#         # Check if the swap would leave the graph space
+#         if edge1[0] == edge2[1]:  # Would create a self-loop
+#             continue
+#         if edge2[0] == edge1[1]:  # Would create a self-loop
+#             continue
+#         if not np.isclose(adjacency_matrix[edge1[0], edge2[1]], 0):  # Would create a parallel edge
+#             continue
+#         if not np.isclose(adjacency_matrix[edge2[0], edge1[1]], 0):  # Would create a parallel edge
+#             continue
+    
+#         # Perform the swap
+#         adjacency_matrix[edge1[0], edge1[1]] = 0
+#         adjacency_matrix[edge2[0], edge2[1]] = 0
+
+#         if not directed:
+#             if edge1[0] > edge2[1]:
+#                 edge1[0], edge2[1] = edge2[0], edge1[1]
+#             if edge2[0] > edge1[1]:
+#                 edge2[0], edge1[1] = edge1[0], edge2[1]
+#             if np.random.rand() < 0.5:
+#                 weight1, weight2 = weight2, weight1
+#             adjacency_matrix[edge1[0], edge2[1]] = weight1
+#             adjacency_matrix[edge2[0], edge1[1]] = weight2
+#         else:
+#             if preserve_out_strength:
+#                 adjacency_matrix[edge1[0], edge2[1]] = weight2
+#                 adjacency_matrix[edge2[0], edge1[1]] = weight1
+#             else:
+#                 adjacency_matrix[edge1[0], edge2[1]] = weight1
+#                 adjacency_matrix[edge2[0], edge1[1]] = weight2
